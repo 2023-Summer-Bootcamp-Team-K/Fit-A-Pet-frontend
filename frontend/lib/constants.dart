4cb906e998ed3ed426858cb7ed4d008c4601f980
moanuna <xodh0707@naver.com>
2023-07-10 14:35:51 +0900

    [#5]Feat: 메인페이지 구성

---
+++ frontend/lib/constants.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+// Colors (0xFF붙이기)
+const kTextColor = Color(0xFFFEFEFE);
+const kBackgroundColor = Color(0xFFFEFEFE);
+const kTitleTextColor = Color(0xFF303030);
+const kBodyTextColor = Color(0xFF4B4B4B);
+const kTextLightColor = Color(0xFF959595);
+const kInfectedColor = Color(0xFFFF8748);
+const kDeathColor = Color(0xFFFF4848);
+const kRecovercolor = Color(0xFF36C12C);
+// const kPrimaryColor = Color(0xFF3382CC); //파랑
+const kPrimaryColor = Color(0xFFC1CCFF); //연보라블루
+final kShadowColor = Color(0xFFB7B7B7).withOpacity(.16);
+final kActiveShadowColor = Color(0xFF4056C6).withOpacity(.15);
+
+//default value //2:08
+const kDefaultPadding = 20.0;
