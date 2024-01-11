0371791a83e1f8e82afcdbae8ed58e1daa761e0d
seojinJeon <gij0321@naver.com>
2024-01-12 00:38:42 +0900

    [#60]Refactor: notification 수정

--- frontend/lib/components/notification.dart
+++ frontend/lib/components/notification.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:frontend/constant.dart';
 import 'package:frontend/screens/home_screen.dart';
 import 'package:page_transition/page_transition.dart';
-import 'package:timezone/timezone.dart' as tz;
-import 'package:timezone/data/latest.dart' as tz;
 import 'package:time_picker_spinner/time_picker_spinner.dart';
-import 'package:fluttertoast/fluttertoast.dart';
+import 'package:timezone/data/latest.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
 
 class NotificationPage extends StatefulWidget {
   final String petID;
@@ -61,141 +61,163 @@ class _NotificationPageState extends State<NotificationPage> {
           },
         ),
       ),
-      body: Stack(
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              SizedBox(height: 70),
-              Container(
-                child: Text(
-                  '알림 받고 싶은 시간을 설정해 주세요.',
-                  style: TextStyle(
-                    fontSize: 19,
-                    fontWeight: FontWeight.bold,
-                  ),
-                  textAlign: TextAlign.center,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            SizedBox(height: 70),
+            Container(
+              child: Text(
+                '알림 받고 싶은 시간을 설정해 주세요.',
+                style: TextStyle(
+                  fontSize: 19,
+                  fontWeight: FontWeight.bold,
                 ),
+                textAlign: TextAlign.center,
               ),
-              Flexible(
-                flex: 3,
-                child: Padding(
-                  padding: const EdgeInsets.all(25),
-                  child: GridView.count(
-                    crossAxisCount: 3,
-                    mainAxisSpacing: 10,
-                    crossAxisSpacing: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(25),
+              child: Column(
+                children: [
+                  Row(
                     children: [
-                      buildButton(0, '09:00', AndroidNotificationDetails('alarm 1', '1번 푸시'),
+                      buildButton(
+                          0,
+                          '09:00',
+                          AndroidNotificationDetails('alarm 1', '1번 푸시'),
                           DarwinNotificationDetails()),
-                      buildButton(1, '11:00', AndroidNotificationDetails('alarm 2', '2번 푸시'),
+                      buildButton(
+                          1,
+                          '11:00',
+                          AndroidNotificationDetails('alarm 2', '2번 푸시'),
                           DarwinNotificationDetails()),
-                      buildButton(2, '13:00', AndroidNotificationDetails('alarm 3', '3번 푸시'),
-                          DarwinNotificationDetails()),
-                      buildButton(3, '15:00', AndroidNotificationDetails('alarm 4', '4번 푸시'),
-                          DarwinNotificationDetails()),
-                      buildButton(4, '18:00', AndroidNotificationDetails('alarm 5', '5번 푸시'),
-                          DarwinNotificationDetails()),
-                      buildButton(5, '20:00', AndroidNotificationDetails('alarm 6', '6번 푸시'),
+                      buildButton(
+                          2,
+                          '13:00',
+                          AndroidNotificationDetails('alarm 3', '3번 푸시'),
                           DarwinNotificationDetails()),
                     ],
                   ),
-                ),
+                  Row(
+                    children: [
+                      buildButton(
+                          3,
+                          '15:00',
+                          AndroidNotificationDetails('alarm 4', '4번 푸시'),
+                          DarwinNotificationDetails()),
+                      buildButton(
+                          4,
+                          '18:00',
+                          AndroidNotificationDetails('alarm 5', '5번 푸시'),
+                          DarwinNotificationDetails()),
+                      buildButton(
+                          5,
+                          '20:00',
+                          AndroidNotificationDetails('alarm 6', '6번 푸시'),
+                          DarwinNotificationDetails()),
+                    ],
+                  )
+                ],
               ),
-              SizedBox(height: 10),
-              Container(
-                margin: EdgeInsets.fromLTRB(50, 0, 50, 10),
-                child: Column(
-                  children: [
-                    Text(
-                      '직접 시간 설정하기:',
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    SizedBox(height: 20),
-                    PhysicalModel(
-                      elevation: 6,
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(30),
-                      child: Container(
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.white54),
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                        child: TimePickerSpinner(
-                          is24HourMode: false,
-                          isShowSeconds: false,
-                          itemHeight: 80,
-                          normalTextStyle: const TextStyle(
-                            fontSize: 20,
-                          ),
-                          highlightedTextStyle:
-                              const TextStyle(
-                                fontSize: 24, 
-                                color: Color.fromARGB(255, 135, 153, 239),
-                                fontWeight: FontWeight.bold
-                                ),
-                          onTimeChange: (time) {
-                            setState(() {
-                              _selectedTime = TimeOfDay.fromDateTime(time);
-                            });
-                          },
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+            ),
+            SizedBox(height: 10),
+            Container(
+              margin: EdgeInsets.fromLTRB(50, 0, 50, 10),
+              child: Column(
                 children: [
-                  SizedBox(
-                    height: 50,
-                    width: 330,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        if (_selectedButtonIndex != -1 || _selectedTime != null) {
-                          AndroidNotificationDetails androidDetails =
-                              AndroidNotificationDetails('channelId', 'channelName',
-                                  importance: Importance.max, priority: Priority.high);
-                          DarwinNotificationDetails iosDetails = DarwinNotificationDetails();
-                          if (_selectedTime != null) {
-                            _scheduleNotification(androidDetails, iosDetails);
-                            _TimeConfirmation(context, _selectedTime!);
-                          } 
-                        }
-                      },
-                      style: ElevatedButton.styleFrom(
-                        primary: kPrimaryColor,
-                        onPrimary: Colors.white,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30),
-                        ),
+                  Text(
+                    '직접 시간 설정하기:',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  PhysicalModel(
+                    elevation: 6,
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(30),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.white54),
+                        borderRadius: BorderRadius.circular(30),
                       ),
-                      child: Text(
-                        '설정 완료',
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
+                      child: TimePickerSpinner(
+                        is24HourMode: false,
+                        isShowSeconds: false,
+                        itemHeight: 80,
+                        normalTextStyle: const TextStyle(
+                          fontSize: 20,
                         ),
+                        highlightedTextStyle: const TextStyle(
+                            fontSize: 24,
+                            color: Color.fromARGB(255, 135, 153, 239),
+                            fontWeight: FontWeight.bold),
+                        onTimeChange: (time) {
+                          setState(() {
+                            _selectedTime = TimeOfDay.fromDateTime(time);
+                          });
+                        },
                       ),
                     ),
                   ),
                 ],
               ),
-              SizedBox(height: 40),
-            ],
-          ),
-        ],
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 50,
+                  width: 330,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (_selectedButtonIndex != -1 || _selectedTime != null) {
+                        AndroidNotificationDetails androidDetails =
+                            AndroidNotificationDetails(
+                                'channelId', 'channelName',
+                                importance: Importance.max,
+                                priority: Priority.high);
+                        DarwinNotificationDetails iosDetails =
+                            DarwinNotificationDetails();
+                        if (_selectedTime != null) {
+                          _scheduleNotification(androidDetails, iosDetails);
+                          _TimeConfirmation(context, _selectedTime!);
+                        }
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: kPrimaryColor,
+                      onPrimary: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                    ),
+                    child: Text(
+                      '설정 완료',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 40),
+          ],
+        ),
       ),
     );
   }
 
   Widget buildButton(
-      int index, String label, AndroidNotificationDetails androidDetails, DarwinNotificationDetails iosDetails) {
+      int index,
+      String label,
+      AndroidNotificationDetails androidDetails,
+      DarwinNotificationDetails iosDetails) {
     final isSelected = index == _selectedButtonIndex;
     final timeComponents = label.split(':');
     final hour = int.parse(timeComponents[0]);
@@ -229,7 +251,8 @@ class _NotificationPageState extends State<NotificationPage> {
     );
   }
 
-  Future<void> _TimeConfirmation(BuildContext context, TimeOfDay selectedTime) async {
+  Future<void> _TimeConfirmation(
+      BuildContext context, TimeOfDay selectedTime) async {
     return showDialog<void>(
       context: context,
       builder: (BuildContext context) {
@@ -248,19 +271,19 @@ class _NotificationPageState extends State<NotificationPage> {
             ],
           ),
           content: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text('설정한 시간에 알림을 받으시겠습니까?'),
-            SizedBox(height: 10),
-            Text(
-              '선택한 시간: ${selectedTime.format(context)}',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text('설정한 시간에 알림을 받으시겠습니까?'),
+              SizedBox(height: 10),
+              Text(
+                '선택한 시간: ${selectedTime.format(context)}',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-          ],
-        ),
+            ],
+          ),
           actions: <Widget>[
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -274,7 +297,7 @@ class _NotificationPageState extends State<NotificationPage> {
                       decoration: BoxDecoration(
                         color: _isYesButtonPressed
                             ? Color(0xFFC1CCFF).withAlpha(128)
-                            : Colors.white, 
+                            : Colors.white,
                         borderRadius: BorderRadius.circular(10),
                       ),
                       child: TextButton(
@@ -287,8 +310,8 @@ class _NotificationPageState extends State<NotificationPage> {
                         ),
                         onPressed: () {
                           setState(() {
-                          _isYesButtonPressed = true; 
-                          _isNoButtonPressed = false; 
+                            _isYesButtonPressed = true;
+                            _isNoButtonPressed = false;
                           });
                           Navigator.of(context).pop();
                           Fluttertoast.showToast(
@@ -299,8 +322,7 @@ class _NotificationPageState extends State<NotificationPage> {
                               backgroundColor: kPrimaryColor,
                               textColor: Colors.white,
                               webShowClose: true,
-                              fontSize: 16.0
-                          );
+                              fontSize: 16.0);
                         },
                       ),
                     ),
@@ -315,8 +337,8 @@ class _NotificationPageState extends State<NotificationPage> {
                     child: Container(
                       decoration: BoxDecoration(
                         color: _isNoButtonPressed
-                            ? Color(0xFFC1CCFF).withAlpha(128) 
-                            : Colors.white, 
+                            ? Color(0xFFC1CCFF).withAlpha(128)
+                            : Colors.white,
                         borderRadius: BorderRadius.circular(10),
                       ),
                       child: TextButton(
@@ -350,11 +372,12 @@ class _NotificationPageState extends State<NotificationPage> {
     });
   }
 
-  void _scheduleNotification(
-      AndroidNotificationDetails androidDetails, DarwinNotificationDetails iosDetails) async {
+  void _scheduleNotification(AndroidNotificationDetails androidDetails,
+      DarwinNotificationDetails iosDetails) async {
     if (_selectedTime != null) {
       final flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
-      final initSettingsAndroid = AndroidInitializationSettings('@mipmap/ic_launcher');
+      final initSettingsAndroid =
+          AndroidInitializationSettings('@mipmap/ic_launcher');
       final initSettingsIOS = DarwinInitializationSettings();
       final initSettings = InitializationSettings(
         android: initSettingsAndroid,
@@ -393,7 +416,8 @@ class _NotificationPageState extends State<NotificationPage> {
         '기기에 스마트폰을 태그할 시간입니다.',
         scheduledDateTime,
         notificationDetails,
-        uiLocalNotificationDateInterpretation: UILocalNotificationDateInterpretation.absoluteTime,
+        uiLocalNotificationDateInterpretation:
+            UILocalNotificationDateInterpretation.absoluteTime,
         androidAllowWhileIdle: true,
       );
     }
